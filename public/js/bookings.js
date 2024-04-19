9f3a8a2eeecbb55a4ff7c7c1dd5ecd4b3197ac45
JosueBacca77 <josuebacca.sistemas@gmail.com>
2024-04-19 18:40:56 -0300

    ADD: Prepared app for deployment

--- public/js/bookings.js
+++ public/js/bookings.js
@@ -1,28 +1,25 @@
-import axios from 'axios';
-import { showAlert } from './alerts';
+import axios from "axios";
+import { showAlert } from "./alerts";
 const stripe = Stripe(
-      'pk_test_51P5HHMJj56PEqob1Pvry838MaLuKwyVEmIJ6rIDp0WmgjC6IZTeC40o0vCfHg3Lfpl9inukSWimUi6kFq4mSB5h100msw6n19S',
+  "pk_test_51P5HHMJj56PEqob1Pvry838MaLuKwyVEmIJ6rIDp0WmgjC6IZTeC40o0vCfHg3Lfpl9inukSWimUi6kFq4mSB5h100msw6n19S"
 );
 
 export const bookTour = async (tourId) => {
-      try {
-            //1) Get heckout tour from API
-            const res = await axios({
-                  method: 'GET',
-                  url: `http://localhost:3000/api/v1/bookings/checkout-session/${tourId}`,
-            });
-            if (res.data.status === 'success') {
-                  showAlert('success', 'Session created succesfully');
-            }
-            //2) Create checkout form + charge credit card
+  try {
+    //1) Get heckout tour from API
+    const res = await axios({
+      method: "GET",
+      url: `/api/v1/bookings/checkout-session/${tourId}`,
+    });
+    if (res.data.status === "success") {
+      showAlert("success", "Session created succesfully");
+    }
+    //2) Create checkout form + charge credit card
 
-            const purchaseResp = await stripe.redirectToCheckout({
-                  sessionId: res.data.session.id,
-            });
-      } catch (err) {
-            showAlert(
-                  'error',
-                  err?.response?.data?.message ?? 'Error booking tour',
-            );
-      }
+    const purchaseResp = await stripe.redirectToCheckout({
+      sessionId: res.data.session.id,
+    });
+  } catch (err) {
+    showAlert("error", err?.response?.data?.message ?? "Error booking tour");
+  }
 };
--- public/js/login.js
+++ public/js/login.js
@@ -1,39 +1,36 @@
-import axios from 'axios';
-import { showAlert } from './alerts';
+import axios from "axios";
+import { showAlert } from "./alerts";
 
 export const login = async (email, password) => {
-      try {
-            const res = await axios({
-                  method: 'POST',
-                  url: 'http://localhost:3000/api/v1/users/login',
-                  data: {
-                        email,
-                        password,
-                  },
-            });
-            if (res.data.status === 'success') {
-                  showAlert('success', 'Logged in successfully');
-                  window.setTimeout(() => {
-                        location.assign('/');
-                  }, 1500);
-            }
-      } catch (err) {
-            showAlert(
-                  'error',
-                  err?.response?.data?.message ?? 'Error logging in',
-            );
-      }
+  try {
+    const res = await axios({
+      method: "POST",
+      url: "/api/v1/users/login", //Since the client and the API are hosted in the same server, this works, it's a relative path
+      data: {
+        email,
+        password,
+      },
+    });
+    if (res.data.status === "success") {
+      showAlert("success", "Logged in successfully");
+      window.setTimeout(() => {
+        location.assign("/");
+      }, 1500);
+    }
+  } catch (err) {
+    showAlert("error", err?.response?.data?.message ?? "Error logging in");
+  }
 };
 
 export const logout = async () => {
-      try {
-            const res = await axios({
-                  method: 'POST',
-                  url: 'http://localhost:3000/api/v1/users/logout',
-            });
-            if ((res.data.status = 'success')) location.reload(true); //we really want a reload page
-      } catch (err) {
-            showAlert('error', 'Error logging out');
-            console.log(err.response);
-      }
+  try {
+    const res = await axios({
+      method: "POST",
+      url: "/api/v1/users/logout",
+    });
+    if ((res.data.status = "success")) location.reload(true); //we really want a reload page
+  } catch (err) {
+    showAlert("error", "Error logging out");
+    console.log(err.response);
+  }
 };
