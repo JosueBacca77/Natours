fbcc457aa6f20ed447e0f58a27f49e2283527b68
JosueBacca77 <josuebacca.sistemas@gmail.com>
2024-04-21 19:57:52 -0300

    REMOVE: console.log

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -56,7 +56,6 @@ const getCheckoutSession = createAsync(async (req, res, next) => {
 });
 
 const createBookingCheckout = createAsync(async (session) => {
-  console.log("session to create", session);
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_details.email }))
     .id;
