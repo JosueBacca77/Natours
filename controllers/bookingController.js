e354de79d0e8c0e4352c44f741f06996ad4f2147
JosueBacca77 <josuebacca.sistemas@gmail.com>
2024-04-21 19:52:42 -0300

    FIX: fields read from session in webhook

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -58,8 +58,9 @@ const getCheckoutSession = createAsync(async (req, res, next) => {
 const createBookingCheckout = createAsync(async (session) => {
   console.log("session to create", session);
   const tour = session.client_reference_id;
-  const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].unit_amount / 100;
+  const user = (await User.findOne({ email: session.customer_details.email }))
+    .id;
+  const price = session.amount_total / 100;
   await Booking.create({ tour, user, price });
 });
 
