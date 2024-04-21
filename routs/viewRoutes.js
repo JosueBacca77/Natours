8680205830b8581b37796e6736fe2540d37a8868
JosueBacca77 <josuebacca.sistemas@gmail.com>
2024-04-21 19:24:14 -0300

    FIX: / view route

--- routs/viewRoutes.js
+++ routs/viewRoutes.js
@@ -1,28 +1,26 @@
-const express = require('express');
+const express = require("express");
 const {
-      getOverview,
-      getTour,
-      getLoginForm,
-      getAccount,
-      updateUserData,
-      getMyTours,
-} = require('../controllers//viewController');
-const { protect, isLoggedIn } = require('../controllers/authController');
-const { createBookingCheckout } = require('../controllers/bookingController');
+  getTour,
+  getLoginForm,
+  getAccount,
+  updateUserData,
+  getMyTours,
+  getOverview,
+} = require("../controllers//viewController");
+const { protect, isLoggedIn } = require("../controllers/authController");
 
 const viewRouter = express.Router();
 
-//WORK AROUND FOR CREATING BOOKING, dont to use createBookingCheckout here
-viewRouter.get('/', createBookingCheckout, isLoggedIn, getOverview);
+viewRouter.get("/", isLoggedIn, getOverview);
 
-viewRouter.get('/tour/:slug', isLoggedIn, getTour);
+viewRouter.get("/tour/:slug", isLoggedIn, getTour);
 
-viewRouter.get('/login', isLoggedIn, getLoginForm);
+viewRouter.get("/login", isLoggedIn, getLoginForm);
 
-viewRouter.get('/me', protect, getAccount);
+viewRouter.get("/me", protect, getAccount);
 
-viewRouter.post('/submit-user-data', protect, updateUserData);
+viewRouter.post("/submit-user-data", protect, updateUserData);
 
-viewRouter.get('/my-tours', protect, getMyTours);
+viewRouter.get("/my-tours", protect, getMyTours);
 
 module.exports = viewRouter;
